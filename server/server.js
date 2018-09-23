e66738cd6bee66b5d09db68bbe09e46e05476955
Saurabh Chouhan <schouhan@aripratech.com>
2018-09-24 00:44:30 +0530

    Modified to separate out react build from server build

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 import Koa from 'koa'
 import config from 'config'
+import koaStatic from 'koa-static'
 
 let app = new Koa()
 
 const port = config.get('Server.port');
 
-app.use(async ctx => {
-    ctx.body = 'Hello World';
-});
+app.use((ctx) => {
+        ctx.body = 'Hello World'
+    }
+)
 
 app.listen(port, () => {
     console.log('Server started on %s', port)
